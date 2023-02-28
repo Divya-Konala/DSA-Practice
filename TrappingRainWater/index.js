3f9b4eb7fd2a30982090cc9bb9450bcf217f6b82
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-02-28 19:16:45 +0530

    trapping rain water

--- TrappingRainWater/index.js
+++ TrappingRainWater/index.js
@@ -1,29 +1,22 @@
 //Best Implementation
 var trap = function (height) {
-  if (height == null || height.length == 0) {
-    return 0;
-  }
+  if (height == null || height.length == 0) return 0;
 
+  let n = height.length;
   let left = 0,
-    right = height.length - 1;
-  let left_max = 0,
-    right_max = 0;
+    right = n - 1;
+  let leftMax = 0,
+    rightMax = 0;
   let water = 0;
 
   while (left < right) {
     if (height[left] < height[right]) {
-      if (height[left] >= left_max) {
-        left_max = height[left];
-      } else {
-        water += left_max - height[left];
-      }
+      if (height[left] >= leftMax) leftMax = height[left];
+      else water += leftMax - height[left];
       left++;
     } else {
-      if (height[right] >= right_max) {
-        right_max = height[right];
-      } else {
-        water += right_max - height[right];
-      }
+      if (height[right] >= rightMax) rightMax = height[right];
+      else water += rightMax - height[right];
       right--;
     }
   }
